01d839def9debb4c3b0be2356bcfe6a0b4d51999
Rob Sullivan <robertosullivan5@gmail.com>
2020-08-24 22:57:26 +0100

    tut 5

--- todo-ninja/src/views/Home.vue
+++ todo-ninja/src/views/Home.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
+    <p class="red white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores incidunt tempora voluptatibus nihil animi. Doloribus amet deleniti pariatur assumenda animi et suscipit? Nesciunt, ad ipsum? Unde voluptatem dicta voluptates in?</p>
+    <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque facere odio eligendi adipisci. Placeat aliquam eum, dolores id cum adipisci similique nisi ipsa sequi ut perferendis expedita alias numquam temporibus.</p>
+    <h1 class="display-4">Massive Display</h1>
+    <h4 class="display-1">Smaller display</h4>
+    <p class="headline font-weight-bold">This is a headline</p>
+    <p class="subheading">This is a sub heading</p>
+    <p class="caption">This is a sub heading</p>
+
+    <v-btn class="pink white--text">Click me</v-btn>
+    <v-btn depressed color="pink">Click me</v-btn>
+    <v-btn text color="pink">Click me</v-btn>
   </div>
 </template>
 
