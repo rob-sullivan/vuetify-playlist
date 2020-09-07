d835bb4207f53758908e81d6a35e7b977634a63a
Rob Sullivan <robertosullivan5@gmail.com>
2020-09-07 22:41:02 +0100

    side bar

--- todo-ninja/src/App.vue
+++ todo-ninja/src/App.vue
@@ -1,16 +1,17 @@
 <template>
-  <v-app>
+  <v-app class="grey lighten-4">
+    <Navbar />
      <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
-
+import Navbar from '@/components/Navbar'
 export default {
   name: 'App',
-
+  components: {Navbar},
   data: () => ({
     //
   }),
