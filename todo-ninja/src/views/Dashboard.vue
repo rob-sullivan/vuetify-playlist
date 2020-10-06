05df41e9e436a81d63246e033342401b66655b4d
Mac <mac@Macs-iMac.local>
2020-10-06 21:36:07 +0100

    grid

--- todo-ninja/src/views/Dashboard.vue
+++ todo-ninja/src/views/Dashboard.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="dashboard">
     <h1 class="subheading grey--text">Dashboard</h1>
+    <v-container class="my-5">
+      <v-card flat class="pa-3">
+        <v-layout row wrap>
+          <v-flex xs12 md6>
+            <div class="caption grey--text">Project Title</div>
+            <div>Create a new website</div>
+          </v-flex>
+        </v-layout>
+      </v-card>
+    </v-container>
   </div>
 </template>
 
--- todo-ninja/src/views/Projects.vue
+++ todo-ninja/src/views/Projects.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="projects">
     <h1 class="subheading grey--text">Projects</h1>
+    <v-container class="my-5">
+      content
+    </v-container>
   </div>
 </template>
 
