fdbcc014fd58d43e9d6c68de4876b8cb1c9b20c3
Mac <mac@Macs-iMac.local>
2020-10-10 21:03:28 +0100

    team

--- todo-ninja/src/views/Dashboard.vue
+++ todo-ninja/src/views/Dashboard.vue
@@ -2,6 +2,27 @@
   <div class="dashboard">
     <h1 class="subheading grey--text">Dashboard</h1>
     <v-container class="my-5">
+      <v-layout row class="mb-3">
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on }">
+            <v-btn small text color="grey" @click="sortBy('title')" v-on="on">
+              <v-icon left small>mdi-folder</v-icon>
+              <span right class="caption text-lowercase">by project name</span>
+            </v-btn>
+          </template>
+          <span>Sorts projects by name</span>
+        </v-tooltip>
+        
+        <v-tooltip top>
+          <template v-slot:activator="{ on }">
+            <v-btn small text color="grey" @click="sortBy('person')" v-on="on">
+              <v-icon left small>mdi-account</v-icon>
+              <span right class="caption text-lowercase">by person</span>
+            </v-btn>
+          </template>
+          <span>Sorts by Person</span>
+        </v-tooltip>
+      </v-layout>
       <v-card flat v-for="project in projects" :key="project.title">
         <v-layout row wrap :class="`pa-3 project ${project.status}`">
           <v-flex xs12 md6>
@@ -41,6 +62,11 @@
           { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'completed'},
           { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue'},
         ]
+      }
+    },
+    methods:{
+      sortBy(prop){
+        this.projects.sort((a,b) => a[prop] < b[prop] ? -1 : 1)
       }
     }
   }
--- todo-ninja/src/views/Team.vue
+++ todo-ninja/src/views/Team.vue
@@ -2,7 +2,23 @@
   <div class="team">
     <h1 class="subheading grey--text">Team</h1>
     <v-container class="my-5">
-      content
+      <v-layout row wrap>
+        <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
+          <v-card flat class="text-xs-center ma-3">
+            <v-responsive class="pt-4">Image goes here</v-responsive>
+            <v-card-text>
+              <div class="subheading">{{person.name}}</div>
+              <div class="grey--text">{{person.role}}</div>
+            </v-card-text>
+            <v-card-action>
+              <v-btn text color="grey">
+                <v-icon small left>mdi-message-text</v-icon>
+                <span>Message</span>
+              </v-btn>
+            </v-card-action>
+          </v-card>
+        </v-flex>
+      </v-layout>
     </v-container>
   </div>
 </template>
@@ -12,6 +28,16 @@
 
   export default {
     name: 'Team',
-
+    data(){
+      return {
+        team: [
+          { name: 'The Net Ninja', role: 'Web developer'},
+          { name: 'Ryu', role: 'Graphic Designer'},
+          { name: 'Chun Li', role: 'Web developer'},
+          { name: 'Gouken', role: 'Social media maverick'},
+          { name: 'Yoshi', role: 'Sales guru'},
+        ]
+      }
+    }
   }
 </script>
