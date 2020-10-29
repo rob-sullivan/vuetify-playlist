3270acacd5b8c848ec915a61cd9a95327537a5cd
Mac <mac@Macs-iMac.local>
2020-10-29 19:33:55 +0000

    avatar & nav profile picture

--- todo-ninja/src/components/Navbar.vue
+++ todo-ninja/src/components/Navbar.vue
@@ -13,6 +13,14 @@
             </v-btn>
         </v-app-bar>
         <v-navigation-drawer app v-model="drawer" class="primary">
+          <v-layout column align-center>
+            <v-flex class="mt-5">
+              <v-avater size="100">
+                <img src="/avatar-1.png" alt="">
+              </v-avater>
+              <p class="white--text subheading mt-1">The Net Ninja</p>
+            </v-flex>
+          </v-layout>
         <v-list>
             <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
             <v-list-item-action>
